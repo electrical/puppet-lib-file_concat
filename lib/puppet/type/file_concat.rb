electrical/puppet-lib-file_concat

commit 143eed41e57c78aa18360a21d108c97512f31979

Add rubocop and fix some stuff

lib/puppet/type/file_concat.rb
@@ -28,10 +28,7 @@ Puppet::Type.newtype(:file_concat) do
   # the file/posix provider will check for the :links property
   # which does not exist
   def [](value)
-    if value == :links
-      return false
-    end
-
+    return false if value == :links
     super
   end
 
@@ -77,17 +74,13 @@ Puppet::Type.newtype(:file_concat) do
       @resource.no_content
     end
 
-    validate do |val|
-      fail "read-only attribute" if !@content_default
+    validate do
+      fail "read-only attribute" unless @content_default
     end
 
     def insync?(is)
       result = super
-
-      if ! result
-        string_file_diff(@resource[:path], @resource.should_content)
-      end
-
+      string_file_diff(@resource[:path], @resource.should_content) if result
       result
     end
 
@@ -109,25 +102,17 @@ Puppet::Type.newtype(:file_concat) do
     @generated_content = ""
     content_fragments = []
 
-    catalog.resources.select do |r|
+    resources = catalog.resources.select do |r|
       r.is_a?(Puppet::Type.type(:file_fragment)) && r[:tag] == self[:tag]
-    end.each do |r|
+    end
 
-     if r[:content].nil? == false
-       fragment_content = r[:content]
-     elsif r[:source].nil? ==false
-       tmp = Puppet::FileServing::Content.indirection.find(r[:source], :environment => catalog.environment)
-       fragment_content = tmp.content
-     end
-
-      content_fragments << [
-        "#{r[:order]}___#{r[:name]}", fragment_content
-      ]
+    resources.each do |r|
+      content_fragments << ["#{r[:order]}___#{r[:name]}", fragment_content(r)]
     end
 
     sorted = content_fragments.sort do |a, b|
       def decompound(d)
-        d.split('___').map { |d| d =~ /^\d+$/ ? d.to_i : d }
+        d.split('___').map { |v| v =~ /^\d+$/ ? v.to_i : v }
       end
 
       decompound(a[0]) <=> decompound(b[0])
@@ -138,13 +123,23 @@ Puppet::Type.newtype(:file_concat) do
     @generated_content
   end
 
-  def stat(dummy_arg = nil)
-    return @stat if @stat and not @stat == :needs_stat
+  def fragment_content(r)
+    if r[:content].nil? == false
+      fragment_content = r[:content]
+    elsif r[:source].nil? == false
+      tmp = Puppet::FileServing::Content.indirection.find(r[:source], :environment => catalog.environment)
+      fragment_content = tmp.content unless tmp.nil?
+    end
+    fragment_content
+  end
+
+  def stat(*)
+    return @stat if @stat && !@stat == :needs_stat
     @stat = begin
       ::File.stat(self[:path])
-    rescue Errno::ENOENT => error
+    rescue Errno::ENOENT
       nil
-    rescue Errno::EACCES => error
+    rescue Errno::EACCES
       warning "Could not stat; permission denied"
       nil
     end

.rubocop.yml
@@ -1,0 +1,52 @@
+# Let's not argue over this...
+StringLiterals:
+  Enabled: false
+
+# I can't find a reason for raise vs fail.
+SignalException:
+  Enabled: false
+
+# I can't find a reason to prefer 'map' when 'collect' is what I mean.
+# I'm collecting things from a list. Maybe someone can help me understand the
+# semantics here.
+CollectionMethods:
+  Enabled: false
+
+# Why do you even *SEE* trailing whitespace? Because your editor was
+# misconfigured to highlight trailing whitespace, right? Maybe turn that off?
+# ;)
+TrailingWhitespace:
+  Enabled: false
+
+# Line length is another weird problem that somehow in the past 40 years of
+# computing we don't seem to have solved. It's a display problem :(
+LineLength:
+  Max: 9000
+
+# %w() vs [ "x", "y", ... ]
+# The complaint is on lib/pleaserun/detector.rb's map of OS=>Runner,
+# i'll ignore it.
+WordArray:
+  MinSize: 5
+
+# A 20-line method isn't too bad.
+MethodLength:
+  Max: 20
+
+# Hash rockets (=>) forever. Why? Not all of my hash keys are static symbols.
+HashSyntax:
+  EnforcedStyle: hash_rockets
+
+# I prefer explicit return. It makes it clear in the code that the
+# code author intended to return a value from a method.
+RedundantReturn:
+  Enabled: false
+
+# My view on a readable case statement seems to disagree with 
+# what rubocop wants and it doesn't let me configure it other than
+# enable/disable.
+CaseIndentation:
+  Enabled: false
+
+Style/FileName:
+  Enabled: false

lib/puppet/provider/file_concat/ruby.rb
@@ -1,11 +1,10 @@
 Puppet::Type.type(:file_concat).provide(:ruby, :parent => Puppet::Type.type(:file).provider(:posix)) do
-
   def exists?
     resource.stat ? true : false
   end
 
   def create
-    # FIXME security issue because the file won't necessarily
+    # FIXME: security issue because the file won't necessarily
     # be created with the specified mode/owner/group if they
     # are specified
     send("content=", resource.should_content)
@@ -17,11 +16,15 @@ Puppet::Type.type(:file_concat).provide(:ruby, :parent => Puppet::Type.type(:fil
   end
 
   def content
-    actual = File.read(resource[:path]) rescue nil
+    begin
+      actual = File.read(resource[:path])
+    rescue
+      nil
+    end
     (actual == resource.should_content) ? resource.no_content : actual
   end
-#
-  def content=(value)
+
+  def content=(*)
     File.open(resource[:path], 'w') do |fh|
       fh.print resource.should_content
     end

lib/puppet/type/file_fragment.rb
@@ -1,5 +1,4 @@
 Puppet::Type.newtype(:file_fragment) do
-
   @doc = "Create a file fragment to be used by file_concat.
     the `file_fragment` type creates a file fragment to be collected by file_concat based on the tag.
     The example is based on exported resources.
@@ -39,12 +38,10 @@ Puppet::Type.newtype(:file_fragment) do
   end
 
   validate do
+    # Check if either source or content is set. raise error if none is set
+    fail Puppet::ParseError, "Set either 'source' or 'content'" if self[:source].nil? && self[:content].nil?
 
-   # Check if either source or content is set. raise error if none is set
-   self.fail Puppet::ParseError, "Set either 'source' or 'content'" if self[:source].nil? && self[:content].nil?
-
-   # Check if both are set, if so rais error
-   self.fail Puppet::ParseError, "Can't use 'source' and 'content' at the same time" if !self[:source].nil? && !self[:content].nil?
+    # Check if both are set, if so rais error
+    fail Puppet::ParseError, "Can't use 'source' and 'content' at the same time" if !self[:source].nil? && !self[:content].nil?
   end
-
 end

spec/acceptance/001_basic_spec.rb
@@ -93,7 +93,6 @@ describe "File Concat" do
     describe "single fragment" do
       it 'should run successfully' do
         pp = "
-              $var = 'something'
               file_fragment { 'fragment_1': source => 'puppet:///modules/another/file1', tag => 'mytag' }
               file_concat { 'myfile': tag => 'mytag', path => '/tmp/concat' }
              "
@@ -111,7 +110,6 @@ describe "File Concat" do
     describe "multiple fragments" do
       it 'should run successfully' do
         pp = "
-              $var = 'something2'
               file_fragment { 'fragment_1': source => 'puppet:///modules/another/file1', tag => 'mytag' }
               file_fragment { 'fragment_2': source => 'puppet:///modules/another/file2', tag => 'mytag' }
               file_concat { 'myfile': tag => 'mytag', path => '/tmp/concat' }
@@ -126,6 +124,23 @@ describe "File Concat" do
         its(:content) { should match /contentfile1\ncontentfile2/ }
       end
 
+    end
+
+    describe "non existing fragment" do
+      it 'should run successfully' do
+        pp = "
+              file_fragment { 'fragment_1': source => 'puppet:///modules/another/file3', tag => 'mytag' }
+              file_concat { 'myfile': tag => 'mytag', path => '/tmp/concat' }
+             "
+        apply_manifest(pp, :catch_failures => true)
+        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+
+      end
+
+      describe file('/tmp/concat') do
+        it { should be_file }
+        its(:content) { should match // }
+      end
     end
    
   end

commit 65be634f6f588626cc5762480c91f41796023744

Fix unnecessary notifications
string_file_diff() should really only be called when there actually *are* differences.

Bug was introduced by refactoring in https://github.com/electrical/puppet-lib-file_concat/commit/143eed41e57c78aa18360a21d108c97512f31979

lib/puppet/type/file_concat.rb
@@ -80,7 +80,7 @@ Puppet::Type.newtype(:file_concat) do
 
     def insync?(is)
       result = super
-      string_file_diff(@resource[:path], @resource.should_content) if result
+      string_file_diff(@resource[:path], @resource.should_content) unless result
       result
     end
 